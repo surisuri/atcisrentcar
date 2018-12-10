9e076cba6ec3d7d3fe6433c5afc4f195f68193cb
lks <lks@email.com>
2018-12-10 23:30:43 +0900

    login + welcome 화면 구성

--- state/views.py
+++ state/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from car.models import Car
 
 def listcar(request):
-    return render(request, 'state/listcar.html', {})
+    listcar = Car.objects.order_by('created_date')
+    return render(request, 'state/listcar.html', {'listcar': listcar})
