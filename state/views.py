85b3af59576aa3008f011547d8ff77bec7d63a9d
lks <lks@email.com>
2018-12-12 06:11:42 +0900

    listcar 화면에서 updatecar 화면 전환 시 로그인 상태 체크하는 로직 추가

--- state/views.py
+++ state/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from car.models import Car
 from django.shortcuts import render, redirect, get_object_or_404
-
+from django.contrib.auth.decorators import login_required
 
 def listcar(request):
     listcar = Car.objects.order_by('created_date')
     return render(request, 'state/listcar.html', {'listcar': listcar})
 
+@login_required
 def updatecar(request, pk):
     car = get_object_or_404(Car, pk=pk)
     listcar = Car.objects.order_by('created_date')
