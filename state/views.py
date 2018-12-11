ed19e1e6cfa537df4eb672f9dc6fcc382cd9a979
lks <lks@email.com>
2018-12-11 17:13:52 +0900

    google map 추가하기

--- state/views.py
+++ state/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from car.models import Car
+from django.shortcuts import render, redirect, get_object_or_404
+
 
 def listcar(request):
     listcar = Car.objects.order_by('created_date')
     return render(request, 'state/listcar.html', {'listcar': listcar})
-     
+
+def updatecar(request, pk):
+    car = get_object_or_404(Car, pk=pk)
+    listcar = Car.objects.order_by('created_date')
+    return render(request, 'state/updatecar.html',{'car':car, 'listcar':listcar})
